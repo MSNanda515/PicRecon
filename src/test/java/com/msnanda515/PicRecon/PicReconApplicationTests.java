af685fb26949ecb90e6f94d689073ea479369905
Meharpreet Singh Nanda <msnanda515@gmail.com>
2021-09-15 19:09:55 -0600

    Intial commit

---
+++ src/test/java/com/msnanda515/PicRecon/PicReconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msnanda515.PicRecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicReconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
